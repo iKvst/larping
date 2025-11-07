0c50e28a7d0cc3e87bef0b48979341432c478708
iKvst <trentonbrandenburg56@gmail.com>
2025-11-06 21:21:36 -0500

    Update snake_egg.lua

--- snake_egg.lua
+++ snake_egg.lua
@@ -1,3 +1,9 @@
+--[[
+    skidded by yours truly <3
+]]
+
+-- //
+
 b=game.Players.LocalPlayer.PlayerGui.PremiumShop.Frame.Pets.Pet.Pet.Buy
 setupvalue(getconnections(b.MouseButton1Click)[1].Function, 3, 344286613)
 -- then just go on the shop and click to purchase a pet
