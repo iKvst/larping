5e6ce612b25d935339fed03458b2e523080cc3cc
iKvst <trentonbrandenburg56@gmail.com>
2025-11-06 20:00:41 -0500

    Create inf_equip.lua

---
+++ inf_equip.lua
@@ -0,0 +1,72 @@
+-- skidded by yours truly <3
+local alt = ''
+--//
+function getsave()
+	return workspace.__REMOTES.Core["Get Stats"]:InvokeServer().Save
+end
+function getpets()
+	return getsave().Pets
+end
+function getequippedpets()
+	local t = {}
+	for _,p in pairs(getpets()) do
+		if p.e then
+			t[#t+1]=p
+		end
+	end
+	return t
+end
+local mp = getsave().MaxPets
+local T = workspace.__REMOTES.Game.Trading
+local IR = workspace.__REMOTES.Game.Inventory
+local ge = false
+local con
+repeat task.wait()
+	if con then con:Disconnect() con = nil end -- reset connection
+	local lastTradeId = nil
+        
+	con = game:FindFirstChild('Trade Update', true).OnClientEvent:Connect(function(id, data, operation)
+		lastTradeId = id
+	end)
+	repeat task.wait(.25) until T:InvokeServer("InvSend", game.Players[alt]) == true
+        
+	repeat task.wait() until lastTradeId
+	local TotalIn = 0
+	local TotalE = #getequippedpets()
+	print(TotalE)
+	for _, p in pairs(getequippedpets()) do
+		task.spawn(function()
+                	T:InvokeServer("Add", lastTradeId, p.id)
+                	TotalIn = TotalIn + 1
+            	end)
+	end
+	print'well'
+	repeat task.wait(.1) until TotalIn == TotalE
+	print'nice'
+	local curr = #getpets()
+        workspace.__REMOTES.Game.Trading:InvokeServer("Ready", lastTradeId)
+	repeat task.wait(.1) until curr ~= #getpets()
+	task.spawn(function()workspace.__REMOTES.Game.Trading:InvokeServer("Cancel", lastTradeId)end)
+
+	local TEquipped = 0
+	local TFoundNonEquipped = 0
+	local absT = 0
+	for _, p in pairs(getpets()) do
+		if not p.e then
+			absT = absT + 1
+			if TFoundNonEquipped ~= mp then
+				TFoundNonEquipped = TFoundNonEquipped + 1
+				task.spawn(function()
+					print(p.id,IR:InvokeServer('Equip', p.id))
+					TEquipped = TEquipped + 1
+				end)
+			end
+		end
+	end 
+	print'h'
+	repeat task.wait() until TEquipped == TFoundNonEquipped
+	print'ok'
+	ge = (absT==TFoundNonEquipped)
+	repeat task.wait(.1) until curr <= #getpets()
+	print'restart'
+until #getequippedpets() == 0 or ge
