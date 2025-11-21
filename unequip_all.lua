820b39a47cedd7183dada390f191855455fcf8e8
Kast <trentonbrandenburg56@gmail.com>
2025-11-21 16:46:23 -0500

    Update unequip_all.lua

--- unequip_all.lua
+++ unequip_all.lua
@@ -1,13 +1,38 @@
---[[
-    skidded by yours truly <3
-]]
+local Players = game:GetService("Players")
+local Debris = workspace:WaitForChild("__DEBRIS")
+local PetsFolder = Debris:WaitForChild("Pets")
+local Remotes = workspace:WaitForChild("__REMOTES")
+local GameRemotes = Remotes:WaitForChild("Game")
+local PetsRemote = GameRemotes:WaitForChild("Pets")
 
--- //
-
-for _,v in pairs(workspace.__REMOTES.Core['Get Stats']:InvokeServer().Save.Pets)do
-  if v.e then
-    task.spawn(function()
-        workspace.__REMOTES.Game.Inventory:InvokeServer('Unequip', v.id)
-    end)
-  end
+local function removePlayerPets(player)
+local playerFolder = PetsFolder:FindFirstChild(player.Name)
+if playerFolder then
+for _, pet in ipairs(playerFolder:GetChildren()) do
+local args = {
+{
+{
+"Remove",
+player,
+pet.Name
+}
+}
+}
+PetsRemote:FireServer(unpack(args))
 end
+end
+end
+
+Players.PlayerAdded:Connect(function(player)
+wait()
+removePlayerPets(player)
+end)
+
+spawn(function()
+while true do
+for _, player in ipairs(Players:GetPlayers()) do
+removePlayerPets(player)
+end
+wait()
+end
+end)
